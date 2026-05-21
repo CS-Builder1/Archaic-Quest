d0aeae9f2302c7d88b2e2fe4ffec165d14e77dc6
CS-Builder1 <info@swaveservices.com>
2026-05-21 13:22:21 -0400

    chore: remove duplicate setup subdirectory to avoid class conflicts

--- Archaic-Quest-codex-create-outercaldera-scene-and-player-setup/scripts/core/DebugFlags.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-var show_debug_overlay: bool = true
-var invulnerable_player: bool = false
-var freeze_world_state: bool = false
-var log_combat_events: bool = true
-
-func toggle_debug_overlay() -> void:
-	show_debug_overlay = !show_debug_overlay
