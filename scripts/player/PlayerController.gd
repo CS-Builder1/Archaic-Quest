c9d39de5c00e7d275256a6eb128a65905faf1da1
CS-Builder1 <info@swaveservices.com>
2026-05-21 18:15:46 -0400

    Add M5 debug class switching and overlay state output

--- scripts/player/PlayerController.gd
+++ scripts/player/PlayerController.gd
@@ -9,20 +9,43 @@ class_name PlayerController
 @onready var camera: Camera2D = $Camera2D
 @onready var input_intent: PlayerInputIntent = $PlayerInputIntent
 
+const CLASS_BARBARIAN := "Barbarian"
+const CLASS_ARCHER := "Archer"
+const CLASS_MAGE := "Mage"
+
+const CLASS_ORDER := [CLASS_BARBARIAN, CLASS_ARCHER, CLASS_MAGE]
+const CLASS_WEAPONS := {
+	CLASS_BARBARIAN: ["Heavy Axe", "Maul"],
+	CLASS_ARCHER: ["Longbow", "Shortbow"],
+	CLASS_MAGE: ["Fire Staff", "Ember Focus"]
+}
+
 var facing_direction: Vector2 = Vector2.RIGHT
 var state: String = "IDLE"
 var dodge_timer: float = 0.0
 var recovery_timer: float = 0.0
 var last_move_vector: Vector2 = Vector2.RIGHT
+var active_class: String = CLASS_BARBARIAN
+var active_weapon_index: int = 0
+var rage: float = 0.0
+var focus: float = 100.0
+var mana: float = 100.0
 
 func _physics_process(delta: float) -> void:
 	input_intent.collect_intent(camera)
 	_update_facing()
+	_handle_class_intents()
 	_update_state(delta)
 	move_and_slide()
 
-	if Input.is_action_just_pressed("reload_test_scene"):
+	if input_intent.wants_reload_scene:
 		get_tree().reload_current_scene()
+
+	if input_intent.wants_secondary:
+		_swap_weapon()
+
+	if input_intent.wants_primary:
+		_process_attack()
 
 func _update_facing() -> void:
 	var to_mouse := input_intent.aim_world_position - global_position
@@ -58,3 +81,76 @@ func _update_state(delta: float) -> void:
 		state = "DODGING"
 		dodge_timer = dodge_duration
 		velocity = last_move_vector * dodge_speed
+
+func _handle_class_intents() -> void:
+	if input_intent.wants_switch_class_1:
+		_set_class(CLASS_BARBARIAN)
+	elif input_intent.wants_switch_class_2:
+		_set_class(CLASS_ARCHER)
+	elif input_intent.wants_switch_class_3:
+		_set_class(CLASS_MAGE)
+	elif input_intent.wants_cycle_class:
+		var next_index := (CLASS_ORDER.find(active_class) + 1) % CLASS_ORDER.size()
+		_set_class(CLASS_ORDER[next_index])
+
+func _set_class(new_class: String) -> void:
+	if active_class == new_class:
+		return
+	active_class = new_class
+	active_weapon_index = 0
+	EventBus.debug_message.emit("Class switched to %s. Active weapon: %s" % [active_class, get_active_weapon()])
+
+func _swap_weapon() -> void:
+	var weapons: Array = CLASS_WEAPONS.get(active_class, [])
+	if weapons.is_empty():
+		return
+	active_weapon_index = (active_weapon_index + 1) % weapons.size()
+	EventBus.debug_message.emit("Weapon swapped to %s" % get_active_weapon())
+
+func _process_attack() -> void:
+	var target := _find_attack_target()
+	if target == null:
+		return
+	if target.has_method("take_damage"):
+		target.take_damage(10.0, active_class, get_active_weapon())
+	elif target.has_method("apply_damage"):
+		target.apply_damage(10.0)
+	elif target.has_node("HealthComponent"):
+		var health_component: HealthComponent = target.get_node("HealthComponent")
+		health_component.apply_damage(10.0)
+	_update_resources_on_attack()
+
+func _find_attack_target() -> Node:
+	var world := get_tree().current_scene
+	if world == null:
+		return null
+	if world.has_node("CombatDummy"):
+		return world.get_node("CombatDummy")
+	if world.has_node("Enemy"):
+		return world.get_node("Enemy")
+	return null
+
+func _update_resources_on_attack() -> void:
+	match active_class:
+		CLASS_BARBARIAN:
+			rage = min(rage + 15.0, 100.0)
+		CLASS_ARCHER:
+			focus = max(focus - 10.0, 0.0)
+		CLASS_MAGE:
+			mana = max(mana - 12.0, 0.0)
+
+func get_active_weapon() -> String:
+	var weapons: Array = CLASS_WEAPONS.get(active_class, [])
+	if weapons.is_empty():
+		return "Unarmed"
+	return str(weapons[active_weapon_index])
+
+func get_debug_overlay_lines() -> PackedStringArray:
+	var class_hint := "[1] Barbarian [2] Archer [3] Mage | [C] Cycle | RMB Swap"
+	var resources := "Rage %.0f | Focus %.0f | Mana %.0f" % [rage, focus, mana]
+	return PackedStringArray([
+		"Class: %s" % active_class,
+		"Weapon: %s" % get_active_weapon(),
+		"Switch: %s" % class_hint,
+		"Resources: %s" % resources
+	])
